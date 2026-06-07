8d8b927a5eb714d0efaaff410f9d0a8b70793af5
Johnny <johnnyhoerner@gmx.net>
2026-06-07 17:01:26 +0200

    Grundnavigation mit fünf Tabs erstellt

---
+++ lib/screens/livestream_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LivestreamScreen extends StatelessWidget {
+  const LivestreamScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Livestream'),
+    );
+  }
+}
